772664b0d8fae4ad5bfaae2136abf5c34e37192d
Honeyyy <rathodhoney852003@gmail.com>
2025-12-26 13:04:20 +0530

    update

--- AddDehydtaredFruits.php
+++ AddDehydtaredFruits.php
@@ -16,7 +16,7 @@ $cat_id      = $_POST["cat_id"];
 $description = $_POST["description"];
 
 $exe = pathinfo($_FILES['DehydratedFruitImg']['name'], PATHINFO_EXTENSION);
-$filename = time().random_int(1000,9999).'.'.$exe;
+$filename = time() . random_int(1000, 9999) . '.' . $exe;
 
 $sql = "INSERT INTO tbl_products (name, price, unit, cat_id, description,image)
         VALUES ('$name', '$price', '$unit', '$cat_id', '$description','$filename')";
