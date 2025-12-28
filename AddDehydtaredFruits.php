0119d666dceda8a89e05209bf8c797baa7719fad
Honeyyy <rathodhoney852003@gmail.com>
2025-12-28 12:51:44 +0530

    update

--- AddDehydtaredFruits.php
+++ AddDehydtaredFruits.php
@@ -1,34 +1,21 @@
 <?php
-
 include 'connection.php';
-
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type');
-
 $response = array();
-
 $name        = $_POST["name"];
 $price       = $_POST["price"];
 $unit        = $_POST["unit"];
 $cat_id      = $_POST["cat_id"];
 $description = $_POST["description"];
-
 $exe = pathinfo($_FILES['DehydratedFruitImg']['name'], PATHINFO_EXTENSION);
 $filename = time() . random_int(1000, 9999) . '.' . $exe;
-
 $sql = "INSERT INTO tbl_products (name, price, unit, cat_id, description,image)
         VALUES ('$name', '$price', '$unit', '$cat_id', '$description','$filename')";
-
-move_uploaded_file($_FILES['DehydratedFruitImg']['tmp_name'], 'C:\finalSEM\ShreeHariAdminDashboard\assets\uploads\Dehydrated Fruits/' . $filename);
-
-
-
+move_uploaded_file($_FILES['DehydratedFruitImg']['tmp_name'], './uploads/DehydratedFruits/' . $filename);
 $result = mysqli_query($conn, $sql);
-
 $response['status'] = "true";
-
 echo json_encode($response);
-
 $conn->close();
